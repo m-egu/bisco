09ca41d7f8bc203d69f4a380ef3ae6944a2afded
パソ子forMac <pasokoformac@pasokoforMacnoMacBook-Pro.local>
2017-05-06 18:00:35 +0900

    プロジェクトのコミット

---
+++ src/main/java/com/bisco/controller/BiscoApplication.java
@@ -0,0 +1,12 @@
+package com.bisco.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiscoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiscoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bisco/controller/BiscoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bisco.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BiscoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
